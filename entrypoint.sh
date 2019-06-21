72bcf2eae0c5ff736d3cb2cbd2dc1348d43b8ad2
Michiel Bosman <michiel.bosman@gmail.com>
2019-06-21 14:55:36 +0200

    keep wget quiet to prevent log clutter

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget https://binbin.codedor.be/polar/polar.latest.phar -O /usr/local/bin/polar
+wget --quiet https://binbin.codedor.be/polar/polar.latest.phar -O /usr/local/bin/polar
 chmod +x /usr/local/bin/polar
 
 exec "$@"
