b75ac35745949400445d77b547a44e1589c289f9
Michiel Bosman <michiel.bosman@gmail.com>
2019-06-21 13:00:12 +0200

    verbose wget in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget --quiet https://binbin.codedor.be/polar/polar.latest.phar -O /usr/local/bin/polar
+wget https://binbin.codedor.be/polar/polar.latest.phar -O /usr/local/bin/polar
 chmod +x /usr/local/bin/polar
 
 exec "$@"
